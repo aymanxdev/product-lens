2b48353aa11217b3fa81cd7d24b64484d80fbe4c
aymanxdev <ayman.bacc@gmail.com>
2023-07-23 15:38:02 +0100

    add different db for testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,25 +5,27 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes";
 
 // Environment variables
-if (process.env.NODE_ENV === 'production') {
-    dotenv.config({ path: '.env.production' });
-  } else {
-    dotenv.config({ path: '.env.development' });
-  }
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env.production" });
+} else if (process.env.NODE_ENV === "test") {
+  dotenv.config({ path: ".env.test" });
+} else {
+  dotenv.config({ path: ".env.development" });
+}
 
 // Connect to MongoDB
 const establishMongoConnection = async () => {
-    try {
-        if (!process.env.MONGODB_URI) {
-            throw new Error("MONGODB_URI must be defined");
-        }
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB: ", error);
-        process.exit(1);
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI must be defined");
     }
-}
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error);
+    process.exit(1);
+  }
+};
 
 establishMongoConnection();
 
