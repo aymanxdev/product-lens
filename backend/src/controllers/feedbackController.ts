84f06cc2830c3606165d4f068cf97806394db0bc
aymanxdev <ayman.bacc@gmail.com>
2023-07-30 23:38:14 +0100

    add delete feedback method

--- backend/src/controllers/feedbackController.ts
+++ backend/src/controllers/feedbackController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { IUserRequest } from "../types";
-import Feedback from "../models/feedbackModel";
+import Feedback, { IFeedback } from "../models/feedbackModel";
 import User, { IUser } from "../models/userModel";
 
 export const getUserFeedbacks = async (req: IUserRequest, res: Response): Promise<any> => {
@@ -35,3 +35,27 @@ export const addFeedback = async (req: IUserRequest, res: Response): Promise<voi
         res.status(500).json({ error: "Error adding feedback", errorMessage: error })
     }
 }
+
+
+export const deleteFeedback = async (req: IUserRequest, res: Response) => {
+  try {
+    const feedback: IFeedback | null = await Feedback.findById(req.params.id);
+
+    if (!feedback) {
+      return res.status(404).json({ error: "Feedback not found" });
+    }
+
+    if (feedback.userId !== req.user._id) {
+      return res
+        .status(403)
+        .json({ error: "Not authorized to delete this feedback" });
+    }
+
+    await Feedback.deleteOne({ _id: feedback._id });
+    res.status(200).json({ message: "Feedback deleted" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error deleting feedback", errorMessage: error });
+  }
+};
