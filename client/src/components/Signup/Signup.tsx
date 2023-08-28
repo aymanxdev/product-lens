f57d5e68b45f513b3a121abeac54b5de7005507b
aymanxdev <ayman.bacc@gmail.com>
2023-08-28 19:46:01 +0100

    add signup pag

---
+++ client/src/components/Signup/Signup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Signup = () => {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="Heading">titles</div>
+        <div className="sign-up-form">form</div>
+      </div>
+    </div>
+  );
+};
---
+++ client/src/pages/RegistrationPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RegistrationPage = () => {
+  return (
+    <div className="container">
+      <div>left side</div>
+      <div>right side</div>
+    </div>
+  );
+};
+
+export default RegistrationPage;
