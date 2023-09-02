bf95bbcd0b0494301462a04b52cc24b10348cdab
aymanxdev <ayman.bacc@gmail.com>
2023-09-02 14:57:38 +0100

    modify the page to include show password option

--- client/src/components/Signup/Signup.tsx
+++ client/src/components/Signup/Signup.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import { useState } from 'react';
 import { Formik, Form } from "formik";
 import { validateForm } from "helpers/validation";
 import "./signup.style.scss";
@@ -8,6 +8,8 @@ interface SignupFormValues {
   password: string;
 }
 export const Signup = () => {
+  //   const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="sign-up-container">
       <div className="sign-up-wrapper">
@@ -22,54 +24,72 @@ export const Signup = () => {
               console.log(values);
             }}
             validate={validateForm}
+            validateOnBlur={false}
           >
             {({ values, handleChange, handleBlur, errors, touched }) => (
               <Form>
                 <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Enter your name"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.name}
-                    className="form-input"
-                  />
-                  {touched.name && errors.name ? (
-                    <div className="error-message">{errors.name}</div>
-                  ) : null}
-
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="text"
-                    name="email"
-                    placeholder="Enter your email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    className={touched.email && errors.email ? "has-error" : ""}
-                  />
-                  {touched.email && errors.email ? (
-                    <div className="error-message">{errors.email}</div>
-                  ) : null}
-
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="text"
-                    name="password"
-                    placeholder="Enter your password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    className={
-                      touched.password && errors.password ? "has-error" : ""
-                    }
-                  />
-                  {touched.password && errors.password ? (
-                    <div className="error-message">{errors.password}</div>
-                  ) : null}
-
+                  <div className="form-field">
+                    <label htmlFor="name">Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Enter your name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}
+                      className="form-input"
+                    />
+                    {touched.name && errors.name ? (
+                      <div className="error-message">{errors.name}</div>
+                    ) : null}
+                  </div>
+                  <div className="form-field">
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="text"
+                      name="email"
+                      placeholder="Enter your email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      className={
+                        touched.email && errors.email ? "has-error" : ""
+                      }
+                    />
+                    {touched.email && errors.email ? (
+                      <div className="error-message">{errors.email}</div>
+                    ) : null}
+                  </div>
+                  <div className="form-field">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      //   type={showPassword ? 'text' : 'password'}
+                      type="password"
+                      name="password"
+                      placeholder="Enter your password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}
+                      className={
+                        touched.password && errors.password ? "has-error" : ""
+                      }
+                    />
+                    {/* <div className='show-password-checkbox'>
+                    <input
+                      type='checkbox'
+                      id='show-password'
+                      name='show-password'
+                      checked={showPassword}
+                      onChange={() => setShowPassword(!showPassword)}
+                      
+                    />
+                    <p >Show password</p>
+                    </div> */}
+                    {touched.password && errors.password ? (
+                      <div className="error-message">{errors.password}</div>
+                    ) : null}
+                  </div>
                   <button className="submit-button" type="submit">
                     Sign up
                   </button>
